8b79aea80e9d43ccdc380665207870c58a86f72f
Joel Welling <welling@psc.edu>
2015-05-19 04:04:36 +0000

    Added copyright notice.

--- src/netinterface.py
+++ src/netinterface.py
@@ -1,11 +1,27 @@
 #! /usr/bin/env python
 
+###################################################################################
+# Copyright   2015, Pittsburgh Supercomputing Center (PSC).  All Rights Reserved. #
+# =============================================================================== #
+#                                                                                 #
+# Permission to use, copy, and modify this software and its documentation without #
+# fee for personal use within your organization is hereby granted, provided that  #
+# the above copyright notice is preserved in all copies and that the copyright    #
+# and this permission notice appear in supporting documentation.  All other       #
+# restrictions and obligations are defined in the GNU Affero General Public       #
+# License v3 (AGPL-3.0) located at http://www.gnu.org/licenses/agpl-3.0.html  A   #
+# copy of the license is also provided in the top level of the source directory,  #
+# in the file LICENSE.txt.                                                        #
+#                                                                                 #
+###################################################################################
+
 _rhea_svn_id_ = "$Id$"
 
 from mpi4py import MPI
 import numpy as np
 import types
 from collections import namedtuple
+
 
 def getCommWorld():
     """Provide easy access to the world to packages that don't want to know about MPI"""
