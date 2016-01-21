9879382db53e1d2f46d5bba454f0aee57482a895
Joel Welling <welling@psc.edu>
2016-01-21 23:31:27 +0000

    Converted the 'transitmatrix' travel time data to YAML form and moved it to a more appropriate location.  distance_table_converter.py implements the translation.

---
+++ src/tools/distance_table_converter.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import yaml
+import phacsl.utils.formats.yaml_tools as yaml_tools
+import phacsl.utils.formats.csv_tools as csv_tools
+
+with open('../../models/OrangeCounty/transitmatrix.csv', 'rU') as f:
+    keys, recs = csv_tools.parseCSV(f)
+
+facKeys, facRecs = yaml_tools.parse_all('../../models/OrangeCounty/facilityfactsCurrent')
+facDict = {r['abbrev']: r for r in facRecs}
+
+srcDir = {}
+for rec in recs:
+    src = rec['From']
+    dest = rec['To']
+    secs = rec['Seconds']
+    meters = rec['Meters']
+    cat = facDict[dest]['category']
+    if src not in srcDir:
+        srcDir[src] = {}
+    if cat not in srcDir[src]:
+        srcDir[src][cat] = {}
+    assert dest not in srcDir[src][cat], 'Double entry for %s %s?' % (src, dest)
+    srcDir[src][cat][dest] = {'seconds': secs, 'meters': meters}
+
+with open('../../models/OrangeCounty/transitmatrix.yaml', 'w') as f:
+    yaml.safe_dump(srcDir, f,
+                   default_flow_style=True, indent=4,
+                   encoding='utf-8', width=130, explicit_start=True)
