cf8dfd8b97ae3dad939354b164cc563cfde9cdd2
Shawn Brown <stbrown@psc.edu>
2017-03-03 07:14:57 -0500

    Added a counter to measure the number of admissions that have CRE

--- src/sim/facilitybase.py
+++ src/sim/facilitybase.py
@@ -134,8 +134,11 @@ class Ward(pyrheabase.Ward):
         """An opportunity for derived classes to customize the arrival processing of patients"""
         patientAgent._status = patientAgent._status._replace(justArrived=True)
         self.miscCounters['arrivals'] += 1
+        if patientAgent._status.pthStatus == PthStatus.COLONIZED:
+            self.miscCounters['creArrival'] += 1
         for tP in self.fac.treatmentPolicies:
             tP.handlePatientArrival(self, patientAgent, timeNow)
+
 
     def handlePatientDeparture(self, patientAgent, timeNow):
         """An opportunity for derived classes to customize the departure processing of patients"""
