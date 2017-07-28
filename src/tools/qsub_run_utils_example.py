8ae6b2a4f3706be0da970030bc1db3b1ee2b9478
Jim <jim@xuth.net>
2017-07-28 13:00:24 -0400

    commiting one of the qsub experiment run scripts as an example.

---
+++ src/tools/qsub_run_utils_example.py
@@ -0,0 +1,42 @@
+from qsub_run_utils import RunEnvironment 
+import yaml
+
+
+
+def getExpList():
+    with open("../../models/ChicagoLand/facility_scenario_lists.yaml", 'rU') as f:
+        return yaml.load(f)
+
+
+def makeReplPairs(expNum, expList):
+    expKeys = expList.keys()
+    expKeys.sort()
+
+    rp = []
+    rp.append([['locationsImplementingScenario', 'locAbbrevList'], expList[expKeys[expNum]]])
+
+    rpDict = {'xdro_registry_scenario_constants.yaml': rp,
+              'cre_bundle_scenario_constants.yaml': rp,
+    }
+    return rpDict
+
+def main():
+    runNum = int(sys.argv[1])
+
+    expList = getExpList()
+    expCount = len(expList)
+    
+    expNum = runNum % expCount
+    instNum = int(runNum / expCount)
+    replPairs = makeReplPairs(expNum, expList)
+
+    rEnv = RunEnvironment('ChicagoLand', 'exp_cre_scen', expNum, instNum, 'year_allfac_ChicagoLand.yaml', replPairs)
+    rEnv.buildEverything()
+    rEnv.runSim(reallyRun=False)
+
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
