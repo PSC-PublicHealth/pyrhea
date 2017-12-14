e601eee47fdc566c63406aa3ddc4d7ae0c74b854
blearn <blearn@psc.edu>
2017-12-14 16:46:49 -0500

    improvement to weight_recalibration.py enabling batch runs that generate graphs of population ratios

--- src/sim/weight_recalibration.py
+++ src/sim/weight_recalibration.py
@@ -1,3 +1,25 @@
+############################################################################################
+# file:
+#	weight_recalibration.py
+# Purpose:
+#	This program helps with evaluating and calibrating the transfer weights in pyrhea.
+#	The program runs a batch of pyrhea simulations. The number of runs in a batch
+# 	is set with the option --batch. After a batch is completed, time samples are collect
+# 	on the full batch. Those batch time samples are used to calculate the population
+# 	ratios per facility.
+# Future Work:
+#	This program is intended to be extended to run multiple 'trials', where a trial
+#	consists of a batch run, analysis, then followed by a heuristic step that determines
+#	how to update the transfer weights. This heuristic step has yet to be implemented.
+# Notes:
+#	Matplotlib will likely complain about the pyplot backend being set multiple times.
+#	The backend needs set in this file so it can save graphs to the filesystem.
+############################################################################################
+
+import matplotlib
+matplotlib.use('Agg') # Hack for no display w/ pyplot. Change pyplot backend *before* loading it.
+import matplotlib.pyplot as plt # use plt.savefig(<path>) rather than plt.show()
+
 import sys, os
 from optparse import OptionParser
 import numpy as np
@@ -12,7 +34,6 @@ sys.path.append(os.path.join(cwd, "../tools"))
 from notes_plotter import readFacFiles, checkInputFileSchema
 import collect_time_samples
 
-#TODO: In all functions, change all static facilities to dynamically read from run description
 def calcPopRatios(timeSamples, facDict):
     popRatios={}
     for ent in timeSamples:
@@ -93,11 +114,11 @@ facDict = {}
 def loadTimeSamples(fname):
     with open(fname, 'r') as f:
         timeSamplesJSON = yaml.load(f)
-    #for ent in timeSamplesJSON:
-        #assert ent['time'] == 465
-        #assert ent['abbrev'] in facDict
     #print 'loaded %s' % fname
     return timeSamplesJSON
+
+def _truncateVec(vec):
+    return np.maximum(np.minimum(vec, 2.0), 0.0)
 
 def plotPopRatios(popRatios, facDict, title): 
     plt.xlabel('Actual mean population / meanPop')
@@ -120,7 +141,8 @@ def plotPopRatios(popRatios, facDict, title):
             label=['HOSPITAL', 'LTACH', 'SNF', 'VSNF'])
     plt.legend()
     plt.title(title)
-    plt.show()
+    #plt.show()
+    plt.savefig(title)
     print 'Samples above max:'
     for k, v in popRatios.items():
         if v > 2.0 or v < 0.0:
@@ -154,8 +176,9 @@ def time_sample_collection(run_desc, proto_file, notes_file, outfile_name):
     sys.argv.append(notes_file)
     sys.argv.append("--out")
     sys.argv.append(outfile_name)
-    sys.argv.append("--proto")
-    sys.argv.append(proto_file)
+    if proto_file is not None:
+        sys.argv.append("--proto")
+        sys.argv.append(proto_file)
 
     print(repr(sys.argv))
     collect_time_samples.main()
@@ -163,58 +186,109 @@ def time_sample_collection(run_desc, proto_file, notes_file, outfile_name):
     # restore previous arg list
     sys.argv = stored_args
 
+def batch_time_sample_collection(run_desc, proto_file, outfile_name, batch_size, trial_num):
+    # set run description and --notes argv options
+    stored_args = sys.argv
+    sys.argv = []
+    sys.argv.append("collect_time_samples.py")
+    sys.argv.append(run_desc)
+    for i in range(batch_size):
+        sys.argv.append("--notes")
+        sys.argv.append("notes_t{}_r{}.pkl".format(trial_num,i)) # assumes nomenclature from main()
+    sys.argv.append("--out")
+    sys.argv.append(outfile_name)
+    if proto_file is not None:
+        sys.argv.append("--proto")
+        sys.argv.append(proto_file)
+
+    print(repr(sys.argv))
+    collect_time_samples.main()
+    
+    # restore previous arg list
+    sys.argv = stored_args
+
+
 def main():
     parser = OptionParser(usage="""
-    %prog run_description.yaml
+    %prog run_description.yaml [--proto time_samples_prototype.yaml] [--batch batch_size]
     """)
+    parser.add_option('--proto', action='store', type='string',
+			help="Prototype YAML file to provide sampling times")
+    parser.add_option('--batch', action='store', type='int',
+			help="Number of simulations to run for one batch")
     opts, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('A YAML run description & time_samples prototype is required')
+    if len(args) < 1:
+        parser.error('A YAML run description is required')
     parser.destroy()
 
-    # Init (Build FacDict) TODO: Change from static path names to dynamically read from run description
-    runDesc = checkInputFileSchema(args[0], os.path.join(pyrhea.SCHEMA_DIR, pyrhea.INPUT_SCHEMA))
-    modelDir = runDesc['modelDir']
-    allKeySet, recs = yaml_tools.parse_all(os.path.join(modelDir, 'facilityfacts')) #facilityfactsCurrent2013
-    commKeySet, commRecs = yaml_tools.parse_all(os.path.join(modelDir, 'synthCommunities'))
-    facDict = {r['abbrev']:r for r in recs}
-    for r in commRecs:
-        facDict[r['abbrev']] = r
+    if opts.batch:
+        batch_size = opts.batch
+    else:
+        batch_size = 4
 
-    prevWeightDict = loadInitialWeightDict(modelDir)
-    writeSepFiles(prevWeightDict, 'init', './weights') # store initial weights in 'weights' dir
-    writeWeightsToModel(prevWeightDict, modelDir) # write to modelDir
+    num_trials = 1 # Currently set to 1 because no heursitic step is implemented
+
+    # check prototype file schema (so problems are known before simulations start)
+    if opts.proto:
+        checkInputFileSchema(opts.proto, os.path.join(pyrhea.SCHEMA_DIR, collect_time_samples.OUTPUT_SCHEMA))
+    else:
+        opts.proto = None
+
+    # Init (Build FacDict)
+    inputDict = checkInputFileSchema(args[0], os.path.join(pyrhea.SCHEMA_DIR, pyrhea.INPUT_SCHEMA))
+    modelDir = inputDict['modelDir']
+
+    pyrheautils.PATH_STRING_MAP['MODELDIR'] = modelDir
+    implDir = pyrheautils.pathTranslate(inputDict['facilityImplementationDir'])
+    pyrheautils.PATH_STRING_MAP['IMPLDIR'] = implDir
+
+    facDirList = [pyrheautils.pathTranslate(pth) for pth in inputDict['facilityDirs']]
+    facDict = readFacFiles(facDirList)
+
+    # get initial tranfer weights
+    #prevWeightDict = loadInitialWeightDict(modelDir)
+    #writeSepFiles(prevWeightDict, 'init', './weights') # store initial weights in 'weights' dir
+    #writeWeightsToModel(prevWeightDict, modelDir) # write to modelDir
 
     print("Begin.")
-    for i in range(5): #TODO: Make loop count cli arg; handle signals SIGINT SIGTSTP & cleanly exit
-        print("> run {0}".format(i))
+    for t in range(num_trials):
+        print("> trial {0}".format(t))
+        for r in range(batch_size): #TODO: handle signals SIGINT SIGTSTP & cleanly exit
+            print("> batch run {0}".format(r))
+    
+            ## set new weights
+    
+            ## call pyrhea; provide run description and output file
+            run_pyrhea(args[0], "notes_t{}_r{}.pkl".format(t,r))
+            
+            print("< exiting batch run {0} ...".format(r))
+            # End batch loop
 
-	## set new weights
-
-        ## call pyrhea; provide run description and output file
-        run_pyrhea(args[0], "notes_{0}.pkl".format(i))
-        
         ## call collect_time_samples; provide run description and input notes file
-        time_sample_collection(args[0], args[1], "notes_{0}.pkl".format(i), "time_samples_pass_{0}.yaml".format(i))
+        batch_time_sample_collection(args[0], opts.proto, "time_samples_trial_{0}.yaml".format(t), batch_size, t)
+        #time_sample_collection(args[0], None, "notes_{0}.pkl".format(i), "time_samples_pass_{0}.yaml".format(i))
         
         ## call calcPopRatios
-	timeSamples = loadTimeSamples("time_samples_pass_{0}.yaml".format(i))
+        timeSamples = loadTimeSamples("time_samples_trial_{0}.yaml".format(t))
+        #timeSamples = loadTimeSamples("mytimesamples.yaml")
         popRatios = calcPopRatios(timeSamples, facDict)
-        print("Pop Ratios:")
-        print(repr(popRatios))
+        #print("Pop Ratios:")
+        #print(repr(popRatios))
 
         ## call plotPopRatios TODO: store output to PNG
-        
+        plotPopRatios(popRatios, facDict, 'meanPop_{}'.format(t))
+
         ## call rescaleWeights
-        newWeightDict = rescaleWeights(prevWeightDict, popRatios) 
+        #newWeightDict = rescaleWeights(prevWeightDict, popRatios) 
         
         ## call writeSepFiles
-        writeSepFiles(newWeightDict, i, './weights')
+        #writeSepFiles(newWeightDict, i, './weights')
         
         ## ref new inv_sep files in xfer_with_draw_constants yaml
-        writeWeightsToModel(newWeightDict, modelDir) # write to modelDir
-	
-	print("< exiting run {0} ...".format(i))
+        #writeWeightsToModel(newWeightDict, modelDir) # write to modelDir
+
+        print("< exiting trial {0} ...".format(t))
+        # End trial loop	
 
 if __name__ == "__main__":
 	main()
