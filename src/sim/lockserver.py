9a40f61713520d17a9b175bf009903a7eb6c2e40
Joel Welling <welling@psc.edu>
2018-04-22 18:11:22 -0400

    Modify lockserver.py to use SLURM environment to get a good IP addr

--- src/sim/lockserver.py
+++ src/sim/lockserver.py
@@ -96,12 +96,17 @@ def getIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         # doesn't have to be reachable
-        s.connect(('10.255.255.255', 1))
+        if 'SLURM_LAUNCH_NODE_IPADDR' in os.environ:
+            target = os.environ('SLURM_LAUNCH_NODE_IPADDR')
+        else:
+            target = '10.255.255.255'
+        s.connect((target, 1))
         IP = s.getsockname()[0]
     except:
         IP = '127.0.0.1'
     finally:
         s.close()
+        LOGGER.info('Using IP addr %s', IP)
     return IP
 
 
