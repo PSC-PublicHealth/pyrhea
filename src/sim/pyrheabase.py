ce8b41fe83985f27a0f6307893e57c386b9cda82
Joel Welling <welling@psc.edu>
2018-04-28 17:09:42 -0400

    Temporary diagnostic print in place of assertion that is failing

--- src/sim/pyrheabase.py
+++ src/sim/pyrheabase.py
@@ -318,8 +318,15 @@ class PatientAgent(peopleplaces.Person):
             return None  # signal death
         else:
             facAddrList = self.getCandidateFacilityList(timeNow, tier)
-            assert facAddrList, ("%s tier change %s to %s has no available facilities"
-                                 % (self.name, self.tier, tier))
+            if not facAddrList:
+                print 'FAILING - patient id %s' % str(self.id)
+                if hasattr(self, 'agentHistory'):
+                    print 'patient history %s' % str(self.agentHistory)
+                else:
+                    print 'patient has no history!'
+                print "%s tier change %s to %s has no available facilities" % (self.name, self.tier, tier)
+#             assert facAddrList, ("%s tier change %s to %s has no available facilities"
+#                                  % (self.name, self.tier, tier))
             homeAddr = self._status.homeAddr
 
             # do we want to test for homeAddr in weighted lists?  how would we weight it?
