PSC-PublicHealth/pyrhea

commit 84a05ee86f8c10587f1d53c4cc8e88720efcae03

Merged bcolz note-making with old-style note-making using new DayDataGroup class.

src/sim/bcz_monitor.py
@@ -38,9 +38,18 @@ def addTrackable(key, fn, dtypes):
         logger.warning("TrackableValue keyword collision: %s"%key)
     TrackableValues[key] = (fn, dtypes)
 
+
 def preregisterKey(key):
     global PreregisteredKeys
     PreregisteredKeys.add(key)
+
+
+def generateBCZDirName(baseName, patchId):
+    """
+    A separate function for accessibility
+    """
+    return baseName + '_' + str(patchId)
+
 
 class Monitor(object):
     def __init__(self, filenameBase, patch, stopFn=None, nextStop=None):
@@ -48,7 +57,7 @@ class Monitor(object):
         self.nextStop = nextStop
         self.stopFn = stopFn
         self.filenameBase = filenameBase
-        self.filename = self.filenameBase + '_' + str(patch.patchId)
+        self.filename = generateBCZDirName(self.filenameBase, patch.patchId)
         self.baseColumns = ['fac', 'tier', 'ward', 'day']
         self.dtype = [('fac', 'S20'), ('tier', 'S10'), ('ward', 'uint32'), ('day', 'uint32')]
         for i in xrange(len(PthStatus.names)):
@@ -70,11 +79,11 @@ class Monitor(object):
 
     def registerTrackable(self, key):
         global TrackableValues
-        
+
         # check if this key is already registered
         if key in self.registeredKeys:
             return
-        
+
         fn, dtypes = TrackableValues[key]
         self.registerFields(dtypes, fn)
         self.registeredKeys.add(key)
@@ -88,7 +97,7 @@ class Monitor(object):
         ra = np.recarray((0,), dtype = self.dtype)
         bz.set_nthreads(3)
         self.pthData = bz.ctable(ra, rootdir=self.filename, mode="w", auto_flush=False)
-        
+
     def collectData(self, timeNow):
         """
         traverses each ward of each facility and updates pthData
@@ -111,10 +120,8 @@ class Monitor(object):
                     row.extend(fn(ward, timeNow))
                 self.pthData.append(row)
 
-
         self.flush()
         self.pthDataDF = None
-        pyrheautils.resetMiscCounters(self.patch, timeNow)
 
     def getPthData(self):
         if self.pthDataDF is None:

src/sim/daydata.py
@@ -1,0 +1,60 @@
+#! /usr/bin/env python
+
+###################################################################################
+# Copyright   2015, Pittsburgh Supercomputing Center (PSC).  All Rights Reserved. #
+# =============================================================================== #
+#                                                                                 #
+# Permission to use, copy, and modify this software and its documentation without #
+# fee for personal use within your organization is hereby granted, provided that  #
+# the above copyright notice is preserved in all copies and that the copyright    #
+# and this permission notice appear in supporting documentation.  All other       #
+# restrictions and obligations are defined in the GNU Affero General Public       #
+# License v3 (AGPL-3.0) located at http://www.gnu.org/licenses/agpl-3.0.html  A   #
+# copy of the license is also provided in the top level of the source directory,  #
+# in the file LICENSE.txt.                                                        #
+#                                                                                 #
+###################################################################################
+
+'''
+A DayData singleton maintains a cache of dicts, each of which is produced by a generating
+function and valid for one 'day'.  When data for a new day is requested, the DayDataGroup
+uses the generating function to regenerate the dicts.
+
+Created on Nov 9, 2018
+
+@author: welling
+'''
+
+from phacsl.utils.collections.phacollections import SingletonMetaClass
+
+class DayData(object):
+    def __init__(self, patch, genFun):
+        self.patch = patch
+        self.genFun = genFun
+        self.curDay = None
+        self.curDct = None
+
+    def get(self, timeNow):
+        if self.curDay != timeNow:
+            self.curDct = self.genFun(self.patch, timeNow)
+            self.curDay = timeNow
+        return self.curDct
+
+class DayDataGroup(object):
+    __metaclass__ = SingletonMetaClass
+
+    def __init__(self):
+        self.readyDct = {}
+        self.rawDct = {}
+
+    def add(self, key, genFun):
+        self.rawDct[key] = genFun
+
+    def get(self, patch, key, timeNow):
+        readyKey = (patch.patchId, key)
+        if readyKey not in self.readyDct:
+            assert key in self.rawDct, 'DayData object %s has not been defined' % key
+            self.readyDct[readyKey] = DayData(patch, self.rawDct[key])
+        return self.readyDct[readyKey].get(timeNow)
+
+

src/sim/pyrheautils.py
@@ -228,23 +228,23 @@ def loadModulesFromDir(implementationDir,
             logger.info('Skipping non-python file %s' % fname)
     sys.path.pop()  # drop implementaionDir
 
-_resetCountNeeded = 1
-_resetRequestCount = 0
-_resetRequestTime = None
-
-def resetMiscCounters(patch, timeNow):
-    """
-    stupid hack to reset the miscCounters only after both per day callbacks have been run that need to look at them
-    """
-    global _resetCountNeeded, _resetRequestCount, _resetRequestTime
-    if _resetRequestTime != timeNow:
-        _resetRequestCount = 0
-    _resetRequestCount += 1
-    if _resetRequestCount != _resetCountNeeded:
-        return
-
-    # looks like we should reset everything
-    for fac in patch.allFacilities:
-        for ward in fac.getWards():
-            ward.miscCounters = defaultdict(lambda: 0)
-    
+# _resetCountNeeded = 1
+# _resetRequestCount = 0
+# _resetRequestTime = None
+# 
+# def resetMiscCounters(patch, timeNow):
+#     """
+#     stupid hack to reset the miscCounters only after both per day callbacks have been run that need to look at them
+#     """
+#     global _resetCountNeeded, _resetRequestCount, _resetRequestTime
+#     if _resetRequestTime != timeNow:
+#         _resetRequestCount = 0
+#     _resetRequestCount += 1
+#     if _resetRequestCount != _resetCountNeeded:
+#         return
+# 
+#     # looks like we should reset everything
+#     for fac in patch.allFacilities:
+#         for ward in fac.getWards():
+#             ward.miscCounters = defaultdict(lambda: 0)
+#     

src/sim/week_baseline_OC.yaml
@@ -59,3 +59,10 @@ scenarioWaitDays: 2
   - 'LAMC'
   - 'SCGL'
   - 'NEWO'
+  
+'trackedValues':
+  - 'arrivals'
+  - 'creCounters'
+  - 'newColonizations'
+
+  

commit 2fe65fa77cbc4d44d1ed9724ae80bf74298735b9

Fix to note-taking bug introduced in 84a05ee86f8. Added outputs to R post-processing.

src/sim/bcz_monitor.py
@@ -72,7 +72,7 @@ class Monitor(object):
 
     def getFilename(self):
         return self.filename
-        
+
     def registerFields(self, dtypes, fn):
         self.registeredFns.append(fn)
         self.dtype.extend(dtypes)
@@ -96,7 +96,8 @@ class Monitor(object):
     def solidifyFields(self):
         ra = np.recarray((0,), dtype = self.dtype)
         bz.set_nthreads(3)
-        self.pthData = bz.ctable(ra, rootdir=self.filename, mode="w", auto_flush=False, chunklen=256*1024)
+        self.pthData = bz.ctable(ra, rootdir=self.filename, mode="w",
+                                 auto_flush=False, chunklen=256*1024)
 
     def collectData(self, timeNow):
         """

src/sim/pyrhea.py
@@ -190,17 +190,17 @@ def buildLocalTierPthDict(patch, timeNow):
 
 def generateLocalTierDictBuilder(counterKey):
     def buildDict(patch, timeNow):
-        facTrtDict = {'day': timeNow, 'format': ['fac', 'tieridx']}
+        facTrtDict = {'day': timeNow, 'format': ['fac', 'tieridx', 'wardidx']}
         assert hasattr(patch, 'allFacilities'), ('patch %s has no list of facilities!'
                                                  % patch.name)
         for fac in patch.allFacilities:
             facPPC = defaultdict(lambda: 0)
             for ward in fac.getWards():
-                key = ward.tier
-                facPPC[key] += ward.miscCounters[counterKey]
+                key = (ward.tier, ward.wardNum)
+                facPPC[key] = ward.miscCounters[counterKey]
                 ward.miscCounters[counterKey] = 0.0
-            for key, v in facPPC.items():
-                facTrtDict[(fac.abbrev,key)] = v
+            for (tN, wN), v in facPPC.items():
+                facTrtDict[(fac.abbrev, tN, wN)] = v
         return facTrtDict
     return buildDict
 
@@ -230,7 +230,19 @@ def dictToNote(dct):
     This takes a dict in the format produced by the daily notes routines
     and converts it to the format expected of the Notes output.
     """
-    return {tupleToNoteKey(key): val for key, val in dct.items() if key != 'format'}
+    if 'format' in dct and dct['format'][-1] == 'wardidx':
+        innerD = defaultdict(int)
+        for key, val in dct.items():
+            if isinstance(key, types.TupleType):
+                innerKey = tuple(key[:-1])
+                innerD[innerKey] += val
+            elif key == 'format':
+                innerD['format'] = val[:-1]
+            else:
+                innerD[key] = val
+        return dictToNote(innerD)
+    else:
+        return {tupleToNoteKey(key): val for key, val in dct.items() if key != 'format'}
 
 PER_DAY_NOTES_GEN_DICT = {'occupancy': buildFacOccupancyDict,
                           'localoccupancy': buildLocalOccupancyDict,
@@ -265,17 +277,14 @@ def defineTrackingGroup(gpName, keyTypeL):
         rslt = []
         for key, tp in keyTypeL:  # @UnusedVariable
             dct = DayDataGroup().get(patch, key, timeNow)
-            assert dct['format'] == ['fac', 'tieridx'], (('Cannot build tracking group including'
-                                                          ' %s; wrong format')
-                                                         % key)
+            assert dct['format'] == ['fac', 'tieridx', 'wardidx'], (('Cannot build tracking group'
+                                                                     ' including %s; wrong format')
+                                                                     % key)
             assert dct['day'] == timeNow, (('Tracked quantity entry generator returned wrong date'
                                             ' day (%s vs %s)')
                                            % (dct['day'], timeNow))
-            tplKey = (ward.fac.abbrev, ward.tier)
-            if tplKey in dct:
-                rslt.append(dct[(ward.fac.abbrev, ward.tier)])
-            else:
-                rslt.append(0)
+            tplKey = (ward.fac.abbrev, ward.tier, ward.wardNum)
+            rslt.append(dct[tplKey] if tplKey in dct else 0)
         return rslt
     bcz_monitor.addTrackable(gpName, extractFun, keyTypeL)
 
@@ -615,7 +624,7 @@ class BurnInAgent(patches.Agent):
 
     def run(self, startTime):
         timeNow = self.sleep(self.burnInDays)  # @UnusedVariable
-        LOGGER.info('burnInAgent is running')
+        LOGGER.info('burnInAgent is running at time %s', timeNow)
         self.noteHolderGroup.clearAll(keepRegex='.*(([Nn]ame)|([Tt]ype)|([Cc]ode)|(_vol)|(occupancy)|(pathogen))')
         # and now the agent exits
 

src/tools/ChicagoLand/taumod_analysis.ipynb
@@ -32,7 +32,7 @@
    "outputs": [],
    "source": [
     "#\n",
-    "# Point this at the run des\n",
+    "# Point this at the run description\n",
     "#\n",
     "runDesc = '/home/welling/git/pyRHEA_github/src/sim/week_run_OC.yaml'\n",
     "\n",

src/tools/compute_costs.py
@@ -720,12 +720,12 @@ def determineOutcomes(nIncidence_,nCarriersCRE_, probInfect_, attribMort_, cGlov
                                         + (1.0-pICU)*nCases*nContactsDay) \
                                         * ggr*LOS_NVAP),
                                        targetYear_)
-            
+
             outcomeCosts[k]['thirdParty Costs'] = thirdPartyCosts
-                        
+
     return outcomeCosts
-    
-    
+
+
 def extractNewColonized(abbrev,specialDict,burninDays):
     ncList = getTimeSeriesList(abbrev,specialDict,'localtiernewcolonized')
     print ncList
@@ -736,7 +736,7 @@ def extractNewColonized(abbrev,specialDict,burninDays):
         print dayIndex
         for tpl, curve in curves.items():
             sums[CareTier.names[tpl]] = sum(curve[dayIndex:])
-    
+
     return sums
 
 def extractContactPrecautionDays(abbrev,specialDict,burninDays):
@@ -1090,12 +1090,11 @@ def main():
     nColsReturnTmp = p.map(getNewCols_poolHelper,argsList)
     p.close()
 
-    # import cPickle as pickle
-    # with open('stuff.pkl', 'w') as f:
-    #     pickle.dump(nColsReturnTmp, f)
-    # sys.exit('done')
-    
-    
+#     import cPickle as pickle
+#     with open('stuff.pkl', 'w') as f:
+#         pickle.dump(nColsReturnTmp, f)
+#     sys.exit('done')
+
     #print "HERE!!!!"
     for i in range(0,len(nColsReturnTmp)):
         newColsArray[i] = nColsReturnTmp[i][0]

src/tools/postprocessing/R/command.sh
@@ -1,0 +1,11 @@
+#! /usr/bin/bash -x
+scenario="$1"
+root=${HOME}/sshfshook/201812_Chicago_xdro_bundle_post
+#root=${HOME}/sshfshook/201811_Chicago_baseline_work2_post
+echo 'scenario' $scenario
+
+Rscript ComputeConfidenceIntervals.R \
+	--input_directory $root/$scenario \
+	--output_directory $root/$scenario \
+	--scenario_names ${scenario}_cumulative_counts \
+	--column_names 'Prev within 13,Prev outside 13,Prev within Cook,Prev outside Cook,Prev target,Prev nonTarget,Prev regionWide,Inc within 13,Inc outside 13,Inc within Cook,Inc outside Cook,Inc target,Inc nonTarget,Inc regionWide'