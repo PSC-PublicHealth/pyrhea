2cbc525fc14062a13210b2ce71aed5a4c27c7ed6
Joel S. Welling <weling@psc.edu>
2019-03-04 14:30:47 -0500

    Run automation scripts get an update.
    cleanup.proto uses find --delete instead of rm -r.  gen_counts_parallel and gen_csvfiles respect xdro_facs.
    runs_by_array uses --disableNotes.  generate_subdirs.bash uses new gen_facility_yaml.  survey.bash
    respects single- vs. double-# prefix.

--- src/tools/postprocessing/gen_facility_yaml.py
+++ src/tools/postprocessing/gen_facility_yaml.py
@@ -4,38 +4,64 @@ import os.path
 import sys
 import yaml
 import tools_util as tu
-import pyrheautils
+import pyrheautils as pu
 import csv
 
-simDir = '/pylon5/pscstaff/jleonard/pyrhea/pyrhea/src/sim'
-outBaseDir = '/pylon5/pscstaff/jleonard/pyrhea/output/cre_bundle'
+masterScenarioDir = '/pylon5/pscstaff/jleonard/pyrhea/output/cre_bundle'
 
-scenario = sys.argv[2]
-fn = sys.argv[1]
+def getCapturedLocs(fileName, key):
+    locL = []
+    with open(fileName, 'rU') as f:
+        reader = csv.DictReader(f)
+        for l in reader:
+            if l[key] is not None and l[key] != '':
+                locL.append(l[key])
+    return locL
 
-if not os.path.isabs(fn):
-    fn = os.path.join(outBaseDir, fn)
-inputDict = tu.readModelInputs(fn)
 
-pyrheautils.prepPathTranslations(inputDict)
-fn = pyrheautils.pathTranslate('$(CONSTANTS)/xdro_registry_scenario_constants.yaml')
-if not os.path.isabs(fn):
-    fn = os.path.join(simDir, fn)
-with open(fn, 'rU') as f:
-    xdroJSON = yaml.load(f)
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    scenario = sys.argv[2]
+    fn = sys.argv[1]
+    runYaml = argv[0]
+    topDir = argv[1]
+    bundleDir = argv[2]
+    pyrheaDir = argv[3]
+    workDir = argv[4]
+    scenario = argv[5]
 
-scenariosCSV = os.path.join(outBaseDir, 'scenario_groups.csv')
-locL = []
-with open(scenariosCSV, 'rU') as f:
-    reader = csv.DictReader(f)
-    for l in reader:
-        if l[scenario] != '':
-            locL.append(l[scenario])
+    fn = os.path.join(bundleDir, runYaml)
+    inputDict = tu.readModelInputs(fn)
 
-xdroJSON['locationsImplementingScenario'] = {
-    'prov': 'scenario %s' % scenario,
-    'locAbbrevList': locL
+    pu.prepPathTranslations(inputDict)
+    fn = pu.pathTranslate('$(CONSTANTS)/xdro_registry_scenario_constants.yaml')
+    if not os.path.isabs(fn):
+        fn = os.path.join(os.path.dirname(__file__), fn)
+    with open(fn, 'rU') as f:
+        xdroJSON = yaml.load(f)
+
+    if 'capture_' in scenario:
+        # this is XDRO-style
+        scenariosCSV = os.path.join(topDir,
+                                    'facility_capture_percentages.csv')
+        capture = int(scenario.split('capture_')[1].split('_')[0])
+        key = '{0}% of facilities'.format(capture)
+    else:
+        # this is CRE-style
+        scenariosCSV = os.path.join(masterScenarioDir, 'scenario_groups.csv')
+        key = scenario
+
+    locL = getCapturedLocs(scenariosCSV, key)
+
+    xdroJSON['locationsImplementingScenario'] = {
+        'prov': 'scenario %s' % scenario,
+        'locAbbrevList': locL
     }
 
-yaml.dump(xdroJSON, sys.stdout,
-          indent=4, encoding='utf-8',width=130,explicit_start=True)
+    yaml.dump(xdroJSON, sys.stdout,
+              indent=4, encoding='utf-8',width=130,explicit_start=True)
+
+
+if __name__ == "__main__":
+    main()
